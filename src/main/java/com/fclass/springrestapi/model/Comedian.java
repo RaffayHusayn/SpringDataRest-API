b8a14cbb181b7ac2c3b59cba2b9e589d17d0feb2
Raffay Syed <raffayhusayn@gmail.com>
2021-12-15 19:13:37 -0500

    Created a package com.fclass.springrestapi.model and created a Comedian Entity in it

---
+++ src/main/java/com/fclass/springrestapi/model/Comedian.java
@@ -0,0 +1,58 @@
+package com.fclass.springrestapi.model;
+
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.List;
+
+@Entity
+public class Comedian {
+    @Id
+    int id;
+    String name;
+    int age;
+    @ElementCollection
+    List<String> quoteList;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public List<String> getQuoteList() {
+        return quoteList;
+    }
+
+    public void setQuoteList(List<String> quoteList) {
+        this.quoteList = quoteList;
+    }
+
+    @Override
+    public String toString() {
+        return "Comedian{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", quoteList=" + quoteList +
+                '}';
+    }
+}
