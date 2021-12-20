497c7203bdf3fc6e82f12c0c8e39d3481d9cefca
Raffay Syed <raffayhusayn@gmail.com>
2021-12-19 21:23:41 -0500

    Add data.sql class to populate the RestUser database with entries and password which are hashed in Bcrypt using internet website. Also made changed in the application.properties file to execute SQL statements from the data.sql file

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+delete from Rest_User;
+insert into Rest_User(Id,Username,Password) values(1, "raffay", "$2a$12$mYV102eB8hMxNK5S7aFrFuseS.3p563sj03vhvK8X4dqtbi./TQ/e");
+insert into Rest_User(Id,Username,Password) values(2, "ayesha", "$2a$12$/NmNEcCloscG44tKuOs9duejA3Pzg4o5iUDVMvd1oag.NVAN9q8oO");
