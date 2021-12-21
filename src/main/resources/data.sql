7571a8ad9f1b1437b579fafc96a626f8662356ff
Raffay Syed <raffayhusayn@gmail.com>
2021-12-20 23:01:53 -0500

    Add more users with bcrypted passwords in data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,6 @@
 delete from Rest_User;
 insert into Rest_User(Id,Username,Password) values(1, "raffay", "$2a$12$mYV102eB8hMxNK5S7aFrFuseS.3p563sj03vhvK8X4dqtbi./TQ/e");
 insert into Rest_User(Id,Username,Password) values(2, "ayesha", "$2a$12$/NmNEcCloscG44tKuOs9duejA3Pzg4o5iUDVMvd1oag.NVAN9q8oO");
+insert into Rest_User(Id,Username,Password) values(3, "nabia", "$2a$12$/NmNEcCloscG44tKuOs9duejA3Pzg4o5iUDVMvd1oag.NVAN9q8oO");
+insert into Rest_User(Id,Username,Password) values(4, "zafar", "$2a$12$/NmNEcCloscG44tKuOs9duejA3Pzg4o5iUDVMvd1oag.NVAN9q8oO");
+-- password for raffay: abcd , all others: 1234
